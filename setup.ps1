273cb5b3c69f7d88db797ab645ce186cb42d7bdf
SuperFly233 <36810432+SuperFly233@users.noreply.github.com>
2026-03-30 19:34:30 +0800

    chore: update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -40,15 +40,15 @@ Install-App `
     -DetectPath "%LOCALAPPDATA%\PixPin\PixPin.exe"
 
 Install-App `
-    -Name "OCS Ã¨Â¶ÂÃ¦ÂÂÃ¥Â®Â¢Ã¦ÂÂ·Ã§Â«Â¯" `
+    -Name "OCS Helper" `
     -Url "https://mooc1.chaoxing.com/softdownload/ocsclient/OCS.exe" `
     -Args "/S" `
     -DetectPath "C:\Program Files (x86)\OCS\OCS.exe"
 Write-Host "  [ 打开网站 ]" -ForegroundColor Cyan
 Write-Step "打开网站..."
 @(
-    "https://mooc1.chaoxing.com" # Ã¨Â¶ÂÃ¦ÂÂÃ¥Â­Â¦Ã¤Â¹Â Ã©ÂÂ,
-    "http://10.174.234.251:85/" # Ã¥ÂÂÃ§Â½ÂÃ¥Â®ÂÃ¨Â®Â­Ã¥Â¹Â³Ã¥ÂÂ°
+    "https://mooc1.chaoxing.com" # chaoxing,
+    "http://10.174.234.251:85/" # BJYSoft
 ) | ForEach-Object { Start-Process $_; Start-Sleep -Milliseconds 400 }
 Write-Ok "网站已打开"
 Write-Host ""
